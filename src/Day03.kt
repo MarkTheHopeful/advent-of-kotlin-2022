48fb574f8e912800628cf848cc52a1b007aee3b6
Mark Ipatov <markthehopeful@gmail.com>
2022-12-03 06:29:06 +0100

    Initial solution for Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,16 +1,25 @@
 fun main() {
+    fun priority(c: Char): Int = if (c in 'A'..'Z') c - 'A' + 27 else c - 'a' + 1
+
+    fun countRepetitions(line: String): Int {
+        val n = line.length
+        val second = line.subSequence(n / 2, n).toSet()
+        return line.subSequence(0, n / 2).toSet().filter { it in second }.sumOf { priority(it) }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { countRepetitions(it) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.chunked(3)
+            .sumOf { it.map { it2 -> it2.toSet() }.reduce(Set<Char>::intersect).sumOf { c -> priority(c) } }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
-//    check(part2(testInput) == TODO())
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
