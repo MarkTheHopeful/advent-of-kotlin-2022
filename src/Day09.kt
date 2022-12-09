717c3faac455b44946780f8b1e7b84d267bdd0b2
Mark Ipatov <markthehopeful@gmail.com>
2022-12-09 07:58:24 +0100

    Day09: Refactored

--- src/Day09.kt
+++ src/Day09.kt
@@ -10,54 +10,41 @@ fun main() {
         return max(abs(a.first - b.first), abs(a.second - b.second))
     }
 
-    fun part1(input: List<String>): Int {
+    fun moveOneStep(from: Pair<Int, Int>, to: Pair<Int, Int>): Pair<Int, Int> {
+        return Pair(from.first + (to.first - from.first).sign, from.second + (to.second - from.second).sign)
+    }
+
+    fun applyMove(a: Pair<Int, Int>, dir: Char, len: Int) =
+        Pair(a.first + MOVES[dir]!!.first * len, a.second + MOVES[dir]!!.second * len)
+
+    fun walkAsRope(input: List<String>, ropeLen: Int): Int {
         val visitedPoints: MutableSet<Pair<Int, Int>> = mutableSetOf(Pair(0, 0))
-        var currentHead = Pair(0, 0)
-        var currentTail = Pair(0, 0)
-        input.map { it.split(" ") }.map { (char, lenS) -> Pair(char[0], lenS.toInt()) }.forEach { (char, len) ->
-            currentHead =
-                Pair(currentHead.first + MOVES[char]!!.first * len, currentHead.second + MOVES[char]!!.second * len)
-            while (kingDist(currentHead, currentTail) > 1) {
-                currentTail = Pair(
-                    currentTail.first + (currentHead.first - currentTail.first).sign,
-                    currentTail.second + (currentHead.second - currentTail.second).sign
-                )
-                visitedPoints.add(currentTail)
+        val currentParts = Array(ropeLen) { Pair(0, 0) }
+        input.map { it.split(" ") }.map { (charS, lenS) -> Pair(charS[0], lenS.toInt()) }.forEach { (char, len) ->
+            currentParts[0] = applyMove(currentParts[0], char, len)
+            while (kingDist(currentParts[0], currentParts[1]) > 1) {
+                for (i in 1 until currentParts.size) {
+                    if (kingDist(currentParts[i - 1], currentParts[i]) > 1)
+                        currentParts[i] = moveOneStep(currentParts[i], currentParts[i - 1])
+                    if (i == currentParts.lastIndex) visitedPoints.add(currentParts[i])
+                }
             }
         }
         return visitedPoints.size
     }
 
-    fun part2(input: List<String>): Int {
-        val visitedPoints: MutableSet<Pair<Int, Int>> = mutableSetOf(Pair(0, 0))
-        val currentParts = Array(10) { Pair(0, 0) }
-        input.map { it.split(" ") }.map { (char, lenS) -> Pair(char[0], lenS.toInt()) }.forEach { (char, len) ->
-            currentParts[0] = Pair(
-                currentParts[0].first + MOVES[char]!!.first * len,
-                currentParts[0].second + MOVES[char]!!.second * len
-            )
-            while (kingDist(currentParts[0], currentParts[1]) > 1) {
-                for (i in 1 until currentParts.size) {
-                    if (kingDist(currentParts[i - 1], currentParts[i]) > 1)
-                        currentParts[i] = Pair(
-                            currentParts[i].first + (currentParts[i - 1].first - currentParts[i].first).sign,
-                            currentParts[i].second + (currentParts[i - 1].second - currentParts[i].second).sign
-                        )
-                    if (i == 9)
-                        visitedPoints.add(currentParts[i])
-                }
-            }
+    fun part1(input: List<String>): Int {
+        return walkAsRope(input, 2)
+    }
 
-        }
-        return visitedPoints.size
+    fun part2(input: List<String>): Int {
+        return walkAsRope(input, 10)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-//    check(part1(testInput) == 13)
-    println(part1(testInput))
-    println(part2(testInput))
-//    check(part2(testInput) == 36)
+    check(part1(testInput) == 88)
+    check(part2(testInput) == 36)
 
     val input = readInput("Day09")
     println(part1(input))
