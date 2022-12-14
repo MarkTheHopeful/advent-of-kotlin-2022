4c1b07d393b0be8e2c62a698e1d703fc289191f1
Mark Ipatov <markthehopeful@gmail.com>
2022-12-14 10:10:48 +0100

    Day14: Idk, something like this

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,9 +1,9 @@
 import java.util.*
 
-val dirs: List<Pair<Int, Int>> = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+private val dirs: List<Pair<Int, Int>> = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
 
-operator fun <T> List<List<T>>.get(pair: Pair<Int, Int>) = this[pair.first][pair.second]
-operator fun <E> MutableList<MutableList<E>>.set(xy: Pair<Int, Int>, value: E) {
+private operator fun <T> List<List<T>>.get(pair: Pair<Int, Int>) = this[pair.first][pair.second]
+private operator fun <E> MutableList<MutableList<E>>.set(xy: Pair<Int, Int>, value: E) {
     this[xy.first][xy.second] = value
 }
 
--- src/Day14.kt
+++ src/Day14.kt
@@ -2,9 +2,22 @@ import kotlin.math.sign
 import kotlin.math.min
 import kotlin.math.max
 
-const val EMPTY = 0
-const val BLOCKED = 1
-val MOVES_SAND = listOf(0 to 1, -1 to 1, 1 to 1)
+private const val EMPTY = 0
+private const val BLOCKED = 1
+private val MOVES_SAND = listOf(0 to 1, -1 to 1, 1 to 1)
+
+private operator fun <T> List<List<T>>.get(pair: Pair<Int, Int>) = this[pair.first][pair.second]
+private operator fun <E> MutableList<MutableList<E>>.set(xy: Pair<Int, Int>, value: E) {
+    this[xy.first][xy.second] = value
+}
+
+private operator fun Pair<Int, Int>.minus(other: Pair<Int, Int>): Pair<Int, Int> {
+    return (this.first - other.first) to (this.second - other.second)
+}
+
+private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> {
+    return (this.first + other.first) to (this.second + other.second)
+}
 
 class Field(walls: List<List<Pair<Int, Int>>>, blockVoid: Boolean = false) {
     enum class MoveResultState { MOVED, BLOCKED, FREE }
@@ -12,52 +25,55 @@ class Field(walls: List<List<Pair<Int, Int>>>, blockVoid: Boolean = false) {
 
     val minY = min(0, walls.minOf { it.minOf { it2 -> it2.second } })
     val maxY = walls.maxOf { it.maxOf { it2 -> it2.second } } + if (blockVoid) 2 else 0
-    val minX = min(walls.minOf { it.minOf { it2 -> it2.first } }, min(500, if (blockVoid) 500 - (maxY - minY + 2) else 500))
-    val maxX = max(walls.maxOf { it.maxOf { it2 -> it2.first } }, max(500, if (blockVoid) 500 + (maxY - minY + 2) else 500))
+    val minX =
+        min(walls.minOf { it.minOf { it2 -> it2.first } }, min(500, if (blockVoid) 500 - (maxY - minY + 2) else 500))
+    val maxX =
+        max(walls.maxOf { it.maxOf { it2 -> it2.first } }, max(500, if (blockVoid) 500 + (maxY - minY + 2) else 500))
     val field = MutableList(maxX - minX + 1) { MutableList(maxY - minY + 1) { EMPTY } }
 
     init {
         val walls2 = if (blockVoid) walls + listOf(listOf(Pair(minX, maxY), Pair(maxX, maxY))) else walls
         walls2.forEach {
-            var currentPoint = Pair(it[0].first - minX, it[0].second - minY)
-            field[currentPoint.first][currentPoint.second] = BLOCKED
+            var currentPoint = it[0] - Pair(minX, minY)
+            field[currentPoint] = BLOCKED
             it.forEach { it2 ->
-                while (currentPoint != Pair(it2.first - minX, it2.second - minY)) {
-                    currentPoint = Pair(
-                        currentPoint.first + (it2.first - minX - currentPoint.first).sign,
-                        currentPoint.second + (it2.second - minY - currentPoint.second).sign
+                while (currentPoint != it2 - Pair(minX, minY)) {
+                    currentPoint += Pair(
+                        (it2.first - minX - currentPoint.first).sign, (it2.second - minY - currentPoint.second).sign
                     )
-                    field[currentPoint.first][currentPoint.second] = BLOCKED
+                    field[currentPoint] = BLOCKED
                 }
             }
         }
     }
 
-    fun moveIfPossible(from: Pair<Int, Int>, dir: Pair<Int, Int>): MoveResult {
-        val newPoint = Pair(from.first + dir.first, from.second + dir.second)
-        if (newPoint.first !in (0..(maxX - minX)) || newPoint.second !in (0..(maxY - minY))) return MoveResult(MoveResultState.FREE)
-        if (field[newPoint.first][newPoint.second] == BLOCKED) return MoveResult(MoveResultState.BLOCKED)
+    private fun canMove(from: Pair<Int, Int>, dir: Pair<Int, Int>): MoveResult {
+        val newPoint = from + dir
+        if (newPoint.first !in (0..(maxX - minX)) || newPoint.second !in (0..(maxY - minY)))
+            return MoveResult(MoveResultState.FREE)
+        if (field[newPoint] == BLOCKED) return MoveResult(MoveResultState.BLOCKED)
         return MoveResult(MoveResultState.MOVED, newPoint)
     }
 
     fun dropSand(): Boolean {
-        var sandPoint = Pair(500 - minX, 0 - minY)
-        if (field[sandPoint.first][sandPoint.second] == BLOCKED) return false
-        field[sandPoint.first][sandPoint.second] = BLOCKED
+        var sandPoint = Pair(500, 0) - Pair(minX, minY)
+        if (field[sandPoint] == BLOCKED) return false
+        field[sandPoint] = BLOCKED
         while (true) {
             var movedRes = false
-            MOVES_SAND.forEach {dir ->
-                if (movedRes) return@forEach
-                val moved = moveIfPossible(sandPoint, dir)
-                if (moved.state == MoveResultState.FREE) {
-                    return@dropSand false
-                }
-                if (moved.state == MoveResultState.MOVED) {
-                    movedRes = true
-                    field[sandPoint.first][sandPoint.second] = EMPTY
-                    sandPoint = moved.newPoint!!
-                    field[sandPoint.first][sandPoint.second] = BLOCKED
-                    return@forEach
+            run moves@{
+                MOVES_SAND.forEach { dir ->
+                    val moved = canMove(sandPoint, dir)
+                    if (moved.state == MoveResultState.FREE) {
+                        return@dropSand false
+                    }
+                    if (moved.state == MoveResultState.MOVED) {
+                        movedRes = true
+                        field[sandPoint] = EMPTY
+                        sandPoint = moved.newPoint!!
+                        field[sandPoint] = BLOCKED
+                        return@moves
+                    }
                 }
             }
             if (!movedRes) break
@@ -67,10 +83,7 @@ class Field(walls: List<List<Pair<Int, Int>>>, blockVoid: Boolean = false) {
 }
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        val walls =
-            input.map { it.split(" -> ").map { it2 -> it2.split(",") }.map { (x, y) -> Pair(x.toInt(), y.toInt()) } }
-        val field = Field(walls)
+    fun howMuchSand(field: Field): Int {
         var answer = 0
         while (field.dropSand()) {
             answer++
@@ -78,15 +91,15 @@ fun main() {
         return answer
     }
 
+    fun parseWalls(input: List<String>) =
+        input.map { it.split(" -> ").map { it2 -> it2.split(",") }.map { (x, y) -> Pair(x.toInt(), y.toInt()) } }
+
+    fun part1(input: List<String>): Int {
+        return howMuchSand(Field(parseWalls(input)))
+    }
+
     fun part2(input: List<String>): Int {
-        val walls =
-            input.map { it.split(" -> ").map { it2 -> it2.split(",") }.map { (x, y) -> Pair(x.toInt(), y.toInt()) } }
-        val field = Field(walls, true)
-        var answer = 0
-        while (field.dropSand()) {
-            answer++
-        }
-        return answer
+        return howMuchSand(Field(parseWalls(input), true))
     }
 
     // test if implementation meets criteria from the description, like:
